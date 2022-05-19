d569281e9ccd885019d104596e77dba20c4f7df4
luofei <luofei@trunk.tech>
2022-05-19 17:50:23 +0800

    新增three.js 3D地图

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
+<!--
+ * @Author: JackFly
+ * @since: 2022-05-06 13:38:07
+ * @lastTime: 2022-05-19 11:00:51
+ * @文件相对于项目的路径: /mapsets/src/App.vue
+ * @LastAuthor: Do not edit
+ * @message:
+-->
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+// import MapThree from "./components/MapThree/index.vue";
 import Map from "./components/Map.vue";
 </script>
 
