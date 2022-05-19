278ac36a7e4e07b9d38589fa72474849731ef94b
luofei <luofei@trunk.tech>
2022-05-19 17:50:36 +0800

    性能优化

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,15 +1,15 @@
 <script setup>
-import { onMounted, onUnmounted } from 'vue';
-import * as maptalks from 'maptalks';
-import { GroupGLLayer } from '@maptalks/gl';
-import { GeoJSONVectorTileLayer } from '@maptalks/vt';
-import { GLTFMarker, GLTFLayer } from '@maptalks/gltf-layer';
-import { TransformControl } from '@maptalks/transform-control';
-import * as Stats from 'stats.js';
-import '@maptalks/transcoders.draco';
+import { onMounted, onUnmounted } from "vue";
+import * as maptalks from "maptalks";
+import { GroupGLLayer } from "@maptalks/gl";
+import { GeoJSONVectorTileLayer } from "@maptalks/vt";
+import { GLTFMarker, GLTFLayer } from "@maptalks/gltf-layer";
+import { TransformControl } from "@maptalks/transform-control";
+import * as Stats from "stats.js";
+import "@maptalks/transcoders.draco";
 
-import MapLoader from '../utils/MapLoader';
-import data from '../../public/map.json';
+import MapLoader from "../utils/MapLoader";
+import data from "../../public/map.json";
 
 const state = {
   map: null,
@@ -24,31 +24,31 @@ document.body.appendChild(stats.dom);
 const init = () => {
   // 初始化
   const map = initMap();
-  const groupGLLayer = map.getLayer('groupGLLayer');
+  const groupGLLayer = map.getLayer("groupGLLayer");
   initEvents(map, groupGLLayer);
   state.map = map;
   animate();
 };
 const initMap = () => {
-  const map = maptalks.Map.fromJSON('map', data);
+  const map = maptalks.Map.fromJSON("map", data);
 
   return map;
 };
 const initEvents = (map, groupGLLayer) => {
   const transformControl = new TransformControl();
   transformControl.addTo(map);
-  transformControl.on('transforming', (e) => {
+  transformControl.on("transforming", (e) => {
     const target = transformControl.getTransformTarget();
     target.setTRS(e.translate, e.rotation, e.scale);
   });
 
-  transformControl.on('transformend', (e) => {
-    console.log('操控模型完成事件');
+  transformControl.on("transformend", (e) => {
+    console.log("操控模型完成事件");
   });
 
-  const gltfLayer = groupGLLayer.getLayer('gltf');
+  const gltfLayer = groupGLLayer.getLayer("gltf");
 
-  map.on('click', (e) => {
+  map.on("click", (e) => {
     console.log(e);
     console.log(state.map.toJSON());
     if (state.events.ifEdit) {
@@ -97,5 +97,6 @@ const animate = () => {
 #map {
   width: 100%;
   height: 100%;
+  background-color: rgb(24, 38, 81);
 }
 </style>
